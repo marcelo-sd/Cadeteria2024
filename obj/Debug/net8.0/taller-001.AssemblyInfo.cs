b2b7903f8ff57b1eb2562382c1c849d19e11a0c8
@maquinaDeChelo <msd@gmail.com>
2024-09-05 16:15:09 -0300

    c005-antes-de-merge

--- obj/Debug/net8.0/taller-001.AssemblyInfo.cs
+++ obj/Debug/net8.0/taller-001.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taller-001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11711ed4cf9525ca90a502c7be1813b65815d0d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b1cd323fc01c86626acf7b65664aa013900587")]
 [assembly: System.Reflection.AssemblyProductAttribute("taller-001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taller-001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
